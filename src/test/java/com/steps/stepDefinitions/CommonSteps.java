e411a936c025a4069e49002be013707d63e2cc21
Gulammustufa Momin <gulammustufa.momin@brainvire.com>
2025-01-03 18:28:12 +0530

    first commit

---
+++ src/test/java/com/steps/stepDefinitions/CommonSteps.java
@@ -0,0 +1,12 @@
+package com.steps.stepDefinitions;
+
+import com.steps.cucumber.AbstractSteps;
+import io.appium.java_client.android.AndroidDriver;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+
+public class CommonSteps extends AbstractSteps {
+    private final AndroidDriver driver = testContext().getDriver();
+
+
+}
